24ab6fd837401bd25213f4a3f45655cc00e39bf5
Yael128 <erik.mora@pslgroup.com>
2020-07-14 09:26:25 -0500

    fourth step done

---
+++ src/components/CharComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './CharComponent.css'
+
+function CharComponent() {
+    return (
+        <div className="box">
+
+        </div>
+    );
+}
+
+export default CharComponent;
