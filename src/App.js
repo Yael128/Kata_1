0f45332ae9b13df3293709204d1d40588537a35b
Yael128 <erik.mora@pslgroup.com>
2020-07-14 11:02:14 -0500

    eighth step done

--- src/App.js
+++ src/App.js
@@ -1,42 +1,32 @@
-import React, {Component} from 'react';
+import React, {Component, useState} from 'react';
 import './App.css';
 
 import Validation from './components/Validation';
-import CharComponent from './components/CharComponent';
 import ClickTracker from './components/ClickTracker';
 
-class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            text: ''
-        }
-        this.handleChange = this.handleChange.bind(this)
-        this.removeLetter = this.removeLetter.bind(this)
+function App() {
+    const [text, setText] = useState('')
+
+
+    const handleChange = (e) => {
+        setText(e.target.value)
     }
 
-    handleChange(e) {
-        this.setState({text: e.target.value})
-    }
-
-    removeLetter(index) {
-        const {text} = this.state
+    const removeLetter = (index) => {
         const fSting = text.substring(0, index)
         const sString = text.substring(index + 1, text.length)
 
-        this.setState({text: fSting + sString})
+        setText(fSting + sString)
     }
-
-    render() {
         return (
             <div>
-                <input value={this.state.text} onChange={this.handleChange}></input>
-                <p>{this.state.text.length}</p>
-                <Validation textLength={this.state.text.length}/>
-                <ClickTracker text={this.state.text} removeLetter={this.removeLetter}/>
+                <input value={text} onChange={handleChange}></input>
+                <p>{text.length}</p>
+                <Validation textLength={text.length}/>
+                <ClickTracker text={text} removeLetter={removeLetter}/>
             </div>
         );
     }
-}
+
 
 export default App;
